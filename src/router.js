08f38af0cfdc03586e9abee97a5dfdba32ccbccb
spencerwooo <spencerwoo98@gmail.com>
2019-07-14 20:30:06 +0800

    Feat: add 404 and success view routers

--- src/router.js
+++ src/router.js
@@ -14,12 +14,17 @@ export default new Router({
       component: Home
     },
     {
+      path: '/success',
+      name: 'success',
+      component: () => import(/* webpackChunkName: "about" */ './views/Success.vue')
+    },
+    {
       path: '*',
       name: '404',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/404.vue')
-    }
+    },
   ]
 })
