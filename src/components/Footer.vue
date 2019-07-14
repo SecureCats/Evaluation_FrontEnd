7658193b03451c7f2b813e2d95f208037aab9a0b
spencerwooo <spencerwoo98@gmail.com>
2019-07-14 20:30:34 +0800

    Feat: add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout id="footer-container" align-center justify-start row>
+    <span id="footer">©2019 高校教务处</span>
+    <v-spacer></v-spacer>
+  </v-layout>
+</template>
+
+<style>
+#footer-container {
+  background-color: #404040;
+}
+#footer {
+  color: #ffffff;
+  margin: 12px 24px 12px 24px;
+  font-family: 'Noto Serif SC', serif;
+}
+</style>
