743e102ce393a9ec2676c394a7cf940aa27b822c
FKYnJYQ <loveress01@gmail.com>
2019-07-23 14:50:23 +0800

    use cdn

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/static/pes/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '//cdn.pes.fates.felinae98.cn/static/pes/' : '/',
   outputDir: process.env.NODE_ENV === 'production' ? 'dist/pes' : 'dist',
   configureWebpack: { output: { globalObject: 'this' } }
 }
